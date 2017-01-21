501263dc9e40fb170e5c8e7a0483592cfeca8b79
Monika <jaiswal.monika93@gmail.com>
2017-01-21 22:23:00 +0530

    structure changed

--- config.js
+++ config.js
@@ -1,18 +1,9 @@
 /**
  * Created by monika on 19/1/17.
  */
-import angular from "angular";
-import "angular-animate";
-import "angular-material";
-
-const app = angular.module('app', ['ngMaterial',ngAnimate]);
-// app
-//     .config({
-//     appId: '816677a0-6570-4386-a770-b32c2cfeeb73',
-//         appSecret: '638dcdf2-3ae7-4a0e-8f38-4b87e200d589'
-// });
+// import angular from "angular";
+// const app = angular.module('app', [])
 module.exports = {
     appId: '816677a0-6570-4386-a770-b32c2cfeeb73',
     appSecret: '638dcdf2-3ae7-4a0e-8f38-4b87e200d589'
 };
-export default app;
