35dfa7681fe734166def57a6af74482f20141d72
Monika <jaiswal.monika93@gmail.com>
2017-01-21 22:20:47 +0530

    structure changed

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ import "angular-animate";
 import "angular-material";
 
 const app = angular.module('app', ['ngMaterial',ngAnimate]);
-app
-    .config({
-    appId: '816677a0-6570-4386-a770-b32c2cfeeb73',
-        appSecret: '638dcdf2-3ae7-4a0e-8f38-4b87e200d589'
-});
-// module.exports = {
+// app
+//     .config({
 //     appId: '816677a0-6570-4386-a770-b32c2cfeeb73',
-//     appSecret: '638dcdf2-3ae7-4a0e-8f38-4b87e200d589'
-// };
+//         appSecret: '638dcdf2-3ae7-4a0e-8f38-4b87e200d589'
+// });
+module.exports = {
+    appId: '816677a0-6570-4386-a770-b32c2cfeeb73',
+    appSecret: '638dcdf2-3ae7-4a0e-8f38-4b87e200d589'
+};
 export default app;
